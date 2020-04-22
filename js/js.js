65ce113f451414d9df4a7a0fa18b7bc384ab41f1
Rentonh <rentonhxyc@gmail.com>
2020-04-22 16:21:02 +0930

    minor update before due time
    Signed-off-by: Rentonh <rentonhxyc@gmail.com>

--- js/js.js
+++ js/js.js
@@ -4,24 +4,34 @@ function show__homepage() {
   document.getElementById("recipes__layer").style.display = "none";
   document.getElementById("techniques__layer").style.display = "none";
   document.getElementById("materials__layer").style.display = "none";
+  document.getElementById("start__layer").style.display = "none";
   document.getElementById("homepage__layer").style.display = "block";
 }
 function show__recipes() {
   document.getElementById("homepage__layer").style.display = "none";
+  document.getElementById("techniques__layer").style.display = "none";
   document.getElementById("materials__layer").style.display = "none";
   document.getElementById("start__layer").style.display = "none";
   document.getElementById("recipes__layer").style.display = "block";
 }
 function show__techniques() {
   document.getElementById("homepage__layer").style.display = "none";
+  document.getElementById("recipes__layer").style.display = "none";
+  document.getElementById("materials__layer").style.display = "none";
+  document.getElementById("start__layer").style.display = "none";
   document.getElementById("techniques__layer").style.display = "block";
 }
 function show__materials() {
+  document.getElementById("homepage__layer").style.display = "none";
   document.getElementById("recipes__layer").style.display = "none";
+  document.getElementById("techniques__layer").style.display = "none";
+  document.getElementById("start__layer").style.display = "none";
   document.getElementById("materials__layer").style.display = "block";
 }
 function show__start() {
+  document.getElementById("homepage__layer").style.display = "none";
   document.getElementById("recipes__layer").style.display = "none";
+  document.getElementById("techniques__layer").style.display = "none";
   document.getElementById("materials__layer").style.display = "none";
   document.getElementById("start__layer").style.display = "block";
 }
