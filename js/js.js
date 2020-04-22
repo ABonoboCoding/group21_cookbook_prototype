3af7bd17c9d35401a7d184efa8a700532913d5d1
Rentonh <rentonhxyc@gmail.com>
2020-04-22 16:15:22 +0930

    add sub sub page
    Signed-off-by: Rentonh <rentonhxyc@gmail.com>

--- js/js.js
+++ js/js.js
@@ -3,13 +3,25 @@
 function show__homepage() {
   document.getElementById("recipes__layer").style.display = "none";
   document.getElementById("techniques__layer").style.display = "none";
+  document.getElementById("materials__layer").style.display = "none";
   document.getElementById("homepage__layer").style.display = "block";
 }
 function show__recipes() {
   document.getElementById("homepage__layer").style.display = "none";
+  document.getElementById("materials__layer").style.display = "none";
+  document.getElementById("start__layer").style.display = "none";
   document.getElementById("recipes__layer").style.display = "block";
 }
 function show__techniques() {
   document.getElementById("homepage__layer").style.display = "none";
   document.getElementById("techniques__layer").style.display = "block";
+}
+function show__materials() {
+  document.getElementById("recipes__layer").style.display = "none";
+  document.getElementById("materials__layer").style.display = "block";
+}
+function show__start() {
+  document.getElementById("recipes__layer").style.display = "none";
+  document.getElementById("materials__layer").style.display = "none";
+  document.getElementById("start__layer").style.display = "block";
 }
