b500fd692f8cf68d94a79f5372f918b4671238b5
zhihong zhao <zhihongzhao94@gmail.com>
2020-04-26 13:52:54 +0800

    pic gif vid completed
    Signed-off-by: zhihong zhao <zhihongzhao94@gmail.com>

--- js/js.js
+++ js/js.js
@@ -38,10 +38,16 @@ function show__start() {
 
 function pic_layer(){
     document.getElementById('pic_window').style.display = "block";
-    }
+    document.getElementById('gif_window').style.display = "none";
+    document.getElementById('vid_window').style.display = "none";
+  }
 function gif_layer(){
     document.getElementById('gif_window').style.display = "block";
+    document.getElementById('pic_window').style.display = "none";
+    document.getElementById('vid_window').style.display = "none";
         }
 function vid_layer(){
     document.getElementById('vid_window').style.display = "block";
-                }
+    document.getElementById('gif_window').style.display = "none";
+    document.getElementById('pic_window').style.display = "none";
+  }
